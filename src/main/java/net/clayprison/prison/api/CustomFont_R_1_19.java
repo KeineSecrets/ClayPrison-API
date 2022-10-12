60ba2c9aa13ea8c3bd984ebb6aa161b0ba213b91
keinesecrets1 <miethe.max@web.de>
2022-10-12 19:12:40 +0200

    Initial

---
+++ src/main/java/net/clayprison/prison/api/CustomFont_R_1_19.java
@@ -0,0 +1,47 @@
+package net.clayprison.prison.api;
+
+public enum CustomFont_R_1_19 {
+
+    A("ᴀ", 'a'),
+    B("ʙ", 'b'),
+    C("ᴄ", 'b'),
+    D("ᴅ", 'b'),
+    E("ᴇ", 'b'),
+    F("ғ", 'b'),
+    G("ɢ", 'b'),
+    H("ʜ", 'b'),
+    I("ɪ", 'b'),
+    J("ᴊ", 'b'),
+    K("ᴋ", 'b'),
+    L("ʟ", 'b'),
+    M("ᴍ", 'b'),
+    N("ɴ", 'b'),
+    O("ᴏ", 'b'),
+    P("ᴘ", 'b'),
+    Q("ϙ", 'b'),
+    R("ʀ", 'b'),
+    S("s", 'b'),
+    T("ᴛ", 'b'),
+    U("ᴜ", 'b'),
+    V("ᴠ", 'b'),
+    W("ᴡ", 'b'),
+    X("x", 'b'),
+    Y("ʏ", 'b'),
+    Z("ᴢ", 'z');
+
+    String font;
+    char character;
+
+    CustomFont_R_1_19(String font, char character) {
+        this.font = font;
+        this.character = character;
+    }
+
+    public static String get(String old) {
+        String returned = old;
+        for (CustomFont_R_1_19 font : values()) {
+            returned = returned.replaceAll(font.font, Character.toString(font.character));
+        }
+        return returned;
+    }
+}
